19459efb0231ed4e503df1c75c17501cbbada409
Your Name <lylyby1010@gamil.com>
2021-10-23 21:35:46 +0900

    update db URI to work with MongoDB Atlas

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    //"URI":"mongodb://localhost/book_store"
+    "URI":"mongodb+srv://Kyeongbin:SBtY0usGUIvKtFq2@mongodbserver.stznn.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+};
